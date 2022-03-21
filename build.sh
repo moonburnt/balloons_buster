82fb440653c595aa89110e0898bc69b34a669a66
moonburnt <moonburnt@disroot.org>
2022-03-21 05:15:37 +0200

    dev: add formatting support to build.sh
    - Add "build.sh format" to auto-format src/ and dependencies/engine/
    to match provided .clang-format. It could probably be done better with
    cmake itself, but for now it will do.

--- build.sh
+++ build.sh
@@ -3,10 +3,24 @@ CXX="clang++"
 
 mkdir -p "$BUILD_DIR"
 
-cd "$BUILD_DIR"
+if [ $# -eq 0 ]; then
+    cd "$BUILD_DIR"
 
-export CXX
-cmake ..
-cmake --build . --target clean
-cmake --build .
-mv "./compile_commands.json" "../compile_commands.json"
+    echo "Attempting to build a game"
+    export CXX
+    cmake ..
+    cmake --build . --target clean
+    cmake --build .
+    echo "Updating compile_commands.json"
+    mv "./compile_commands.json" "../compile_commands.json"
+else
+    if [ "$1" == "format" ]; then
+        echo "Attempting to format game's sources"
+        find src/ \( -name '*.cpp' -o -name '*.hpp' \) -exec clang-format -style=file -i {} \;
+        find dependencies/engine/ \( -name '*.cpp' -o -name '*.hpp' \) -exec clang-format -style=file -i {} \;
+    else
+        echo "Invalid build command, check $0 and try again"
+    fi
+fi
+
+echo "Done"
