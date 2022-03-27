729115ef373eda43fd211ce1aa7563b4e651703e
moonburnt <moonburnt@disroot.org>
2022-03-27 02:54:23 +0200

    feat: optional launch flags
    - Add support for optional launch flags.
    - Add launch flag to enable showcase of debug messages.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "engine/utility.hpp"
 #include "menus.hpp"
 #include "shared.hpp"
+#include "spdlog/spdlog.h"
 // For std:max
 #include <algorithm>
+// For string comparison
+#include <cstring>
 
 #define ASSET_PATH "./Assets/"
 static constexpr const char* SPRITE_PATH = ASSET_PATH "Sprites/";
@@ -11,7 +14,17 @@ static constexpr const char* SFX_PATH = ASSET_PATH "SFX/";
 static constexpr const char* SPRITE_FORMAT = ".png";
 static constexpr const char* SFX_FORMAT = ".ogg";
 
-int main() {
+int main(int argc, char* const* argv) {
+    // Processing launch arguments.
+    // For now there is just one - to toggle on debug messages.
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            if (std::strcmp(argv[i], "--debug") == 0) {
+                spdlog::set_level(spdlog::level::debug);
+            }
+        }
+    }
+
     // Window's instance is initialized in shared.cpp
     // window.init() will configure it afterwards
     shared::config.load();
