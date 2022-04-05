b4c46e1748780998fe434e62b751e48362c25cd9
moonburnt <moonburnt@disroot.org>
2022-04-05 23:15:40 +0300

    feat: add SpritesheetAnimation
    - Split Animation to AnimationBase template and Animation that works
    with multiple textures.
    - Add SpritesheetAnimation - an AnimationBase child that turns a pair
    of spritesheet texture + vector of offset rectangles into an animation.
    - Add make_rects() - same as make_sprites(), but it would only create
    rectangles (thus it can be used for SpritesheetAnimation construction).

--- dependencies/engine/sprite.cpp
+++ dependencies/engine/sprite.cpp
@@ -3,7 +3,7 @@
 
 #include <raylib.h>
 
-Sprite::Sprite(Texture2D* _spritesheet, Rectangle _rect)
+Sprite::Sprite(const Texture2D* _spritesheet, Rectangle _rect)
     : spritesheet(_spritesheet)
     , rect(_rect) {
 }
@@ -12,7 +12,7 @@ void Sprite::draw(Vector2 pos) {
     DrawTextureRec(*spritesheet, rect, pos, WHITE);
 }
 
-Sprite make_sprite(Texture2D* spritesheet, Rectangle sprite_rect) {
+Sprite make_sprite(const Texture2D* spritesheet, Rectangle sprite_rect) {
     // I think this will work? Idk how else to ensure if sprite rect is within
     // texture's borders. Maybe I should throw exception instead... TODO.
     ASSERT(is_rect_inside_rect(
@@ -26,66 +26,59 @@ Sprite make_sprite(Texture2D* spritesheet, Rectangle sprite_rect) {
     return Sprite(spritesheet, sprite_rect);
 }
 
-std::vector<Sprite> make_sprites(Texture2D* spritesheet, Vector2 sprite_size) {
+std::vector<Rectangle> make_rects(const Texture2D* spritesheet, Vector2 sprite_size) {
     // Ensuring that spritesheet can be cut to sprites of requested size without
     // leftovers.
     // TODO: maybe add some arg to specify some part of spritesheet to ignore.
+
     ASSERT(spritesheet->width % static_cast<int>(sprite_size.x) == 0);
     ASSERT(spritesheet->height % static_cast<int>(sprite_size.y) == 0);
 
     int horizontal_amount = spritesheet->width / sprite_size.x;
     int vertical_amount = spritesheet->height / sprite_size.y;
 
-    std::vector<Sprite> sprites;
+    std::vector<Rectangle> rects;
 
     // I think this will do? May need to flip these around.
     for (auto current_y = 0; current_y < vertical_amount; current_y++) {
         for (auto current_x = 0; current_x < horizontal_amount; current_x++) {
-            float sprite_x = static_cast<float>(sprite_size.x * current_x);
-            float sprite_y = static_cast<float>(sprite_size.y * current_y);
-            sprites.push_back(Sprite(
-                spritesheet,
-                // May need to flip the last two
-                Rectangle{sprite_x, sprite_y, sprite_size.x, sprite_size.y}));
+            rects.push_back({
+                static_cast<float>(sprite_size.x * current_x),
+                static_cast<float>(sprite_size.y * current_y),
+                sprite_size.x,
+                sprite_size.y});
         }
+    }
+
+    return rects;
+}
+
+std::vector<Sprite> make_sprites(const Texture2D* spritesheet, Vector2 sprite_size) {
+    std::vector<Sprite> sprites;
+
+    for (auto rect : make_rects(spritesheet, sprite_size)) {
+        sprites.push_back(Sprite(spritesheet, rect));
     }
 
     return sprites;
 }
 
 // Animation stuff
-
-Animation::Animation(
-    std::vector<const Texture2D*> _frames, float speed, bool _loop, Vector2 _pos)
-    : timer(speed)
-    , frames(_frames)
-    , loop(_loop)
-    , current_frame(0)
-    , pos(_pos) {
-    timer.start();
-}
-
-Animation::Animation(std::vector<const Texture2D*> _frames, float speed, bool _loop)
-    : Animation(_frames, speed, _loop, {0, 0}) {
-    timer.start();
-}
-
-void Animation::update(float dt) {
-    if (timer.tick(dt)) {
-        if (current_frame + 1 < static_cast<int>(frames.size())) {
-            current_frame++;
-            timer.start();
-        }
-        else if (loop) {
-            current_frame = 0;
-            timer.start();
-        }
-        else {
-            timer.stop();
-        }
-    };
-}
-
 void Animation::draw() {
     DrawTexture(*frames[current_frame], pos.x, pos.y, WHITE);
+}
+
+// Spritesheet animation stuff
+SpritesheetAnimation::SpritesheetAnimation(
+    const Texture2D* _spritesheet,
+    std::vector<Rectangle> _frames,
+    float speed,
+    bool _loop,
+    Vector2 _pos)
+    : AnimationBase<Rectangle>(_frames, speed, _loop, _pos)
+    , spritesheet(_spritesheet) {
+}
+
+void SpritesheetAnimation::draw() {
+    DrawTextureRec(*spritesheet, frames[current_frame], pos, WHITE);
 }
--- dependencies/engine/sprite.hpp
+++ dependencies/engine/sprite.hpp
@@ -6,11 +6,11 @@
 
 class Sprite {
 private:
-    Texture2D* spritesheet;
+    const Texture2D* spritesheet;
     Rectangle rect;
 
 public:
-    Sprite(Texture2D* spritesheet, Rectangle rect);
+    Sprite(const Texture2D* spritesheet, Rectangle rect);
     // TODO: consider using DrawTexturePro under the hood, to allow for angle
     // and colored masks.
     void draw(Vector2 pos);
@@ -18,28 +18,77 @@ public:
 
 // Turn rectangle on specified coordinates into Sprite. If rectangle would be out
 // of bounds - will throw an exception.
-Sprite make_sprite(Texture2D* spritesheet, Rectangle sprite_rect);
+Sprite make_sprite(const Texture2D* spritesheet, Rectangle sprite_rect);
 
-// Turn specified texture into set of same-sized sprites. If spritesheet doesn't
-// cut into specified sized sprites perfectly - will throw an exception.
-std::vector<Sprite> make_sprites(Texture2D* spritesheet, Vector2 sprite_size);
+// Get set of of same-sized rectangles from texture. If spritesheet doesn't
+// cut into these perfectly - will throw an exception.
+std::vector<Rectangle> make_rects(const Texture2D* spritesheet, Vector2 sprite_size);
+// Same as above, but will also convert rects to sprites.
+std::vector<Sprite> make_sprites(const Texture2D* spritesheet, Vector2 sprite_size);
 
 // TODO: maybe make it possible for animation to be both based on spritesheet
-// and separate textures?
-class Animation {
-private:
+// and separate textures? Thus this whole thing will become pointless
+template<typename T> class AnimationBase {
+protected:
     Timer timer;
-    std::vector<const Texture2D*> frames;
+    std::vector<T> frames;
     bool loop;
-    int current_frame;
+    size_t current_frame = 0;
 
 public:
     Vector2 pos;
 
     // Maybe I should make it not vector, but something less memory-consuming? TODO
-    Animation(std::vector<const Texture2D*> frames, float speed, bool loop, Vector2 pos);
-    Animation(std::vector<const Texture2D*> frames, float speed, bool loop);
+    AnimationBase(std::vector<T> _frames, float speed, bool _loop, Vector2 _pos)
+        : timer(speed)
+        , frames(_frames)
+        , loop(_loop)
+        , pos(_pos) {
+        timer.start();
+    }
+    virtual ~AnimationBase() = default;
 
-    void update(float dt);
-    void draw();
+    // Manually current frame to provided value. Doing so will stop timer.
+    void set_current_frame(size_t frame) {
+        current_frame = frame;
+        timer.stop();
+    }
+
+    void update(float dt) {
+        if (timer.tick(dt)) {
+            if (current_frame + 1 < frames.size()) {
+                current_frame++;
+                timer.start();
+            }
+            else if (loop) {
+                current_frame = 0;
+                timer.start();
+            }
+            else {
+                timer.stop();
+            }
+        };
+    }
+
+    virtual void draw() = 0;
+};
+
+class Animation : public AnimationBase<const Texture2D*> {
+public:
+    void draw() override;
+};
+
+class SpritesheetAnimation : public AnimationBase<Rectangle> {
+protected:
+    const Texture2D* spritesheet;
+
+public:
+    SpritesheetAnimation(
+        const Texture2D* spritesheet,
+        std::vector<Rectangle> frames,
+        float speed,
+        bool loop,
+        Vector2 pos);
+
+    void draw() override;
 };
